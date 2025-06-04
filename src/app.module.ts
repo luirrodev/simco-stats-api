0f944135adaa2019343dd3daaad4b27f0e331bbb
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2025-06-04 00:03:29 -0400

    feat: integrate configuration management and database module
    - Added ConfigModule to manage environment variables and configuration.
    - Created a config file to define database connection settings.
    - Implemented DatabaseModule for PostgreSQL connection using TypeORM.
    - Introduced environments configuration for different deployment stages.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+
+import config from './config';
+import { enviroments } from './enviroments';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        (enviroments[process.env.NODE_ENV as string] as string) ||
+        ('dev' as string),
+      isGlobal: true,
+      load: [config],
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+    AuthModule,
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config.ts
@@ -0,0 +1,9 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('config', () => {
+  return {
+    database: {
+      url: process.env.DATABASE_URL,
+    },
+  };
+});
