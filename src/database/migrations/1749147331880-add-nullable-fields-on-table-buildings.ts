8c5248279c86bf15f635e8ad47de9c58af08b6b5
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2025-09-30 15:57:48 -0400

    mejorado migracion

--- src/database/migrations/1749147331880-add-nullable-fields-on-table-buildings.ts
+++ src/database/migrations/1749147331880-add-nullable-fields-on-table-buildings.ts
@@ -1,18 +1,31 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddNullableFieldsOnTableBuildings1749147331880 implements MigrationInterface {
-    name = 'AddNullableFieldsOnTableBuildings1749147331880'
+export class AddNullableFieldsOnTableBuildings1749147331880
+  implements MigrationInterface
+{
+  name = 'AddNullableFieldsOnTableBuildings1749147331880';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "buildings" ALTER COLUMN "kind" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "buildings" ALTER COLUMN "category" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "buildings" ALTER COLUMN "cost" DROP NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "buildings" ALTER COLUMN "kind" DROP NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "buildings" ALTER COLUMN "category" DROP NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "buildings" ALTER COLUMN "cost" DROP NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "buildings" ALTER COLUMN "cost" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "buildings" ALTER COLUMN "category" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "buildings" ALTER COLUMN "kind" SET NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "buildings" ALTER COLUMN "cost" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "buildings" ALTER COLUMN "category" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "buildings" ALTER COLUMN "kind" SET NOT NULL`,
+    );
+  }
 }
