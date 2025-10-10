87ade541c399fd2ba0434f0dd41a6fa70231cfb0
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2025-10-10 11:40:04 -0400

    comentados los endpoints de estadisticas de restaurantes

--- src/restaurant-stats/restaurant-stats.module.ts
+++ src/restaurant-stats/restaurant-stats.module.ts
@@ -5,7 +5,7 @@ import { HttpModule } from '@nestjs/axios';
 import { AuthModule } from '../auth/auth.module';
 import { RestaurantStatEntity } from './entities/restaurant-stat.entity';
 import { RestaurantStatsService } from './services/restaurant-stats.service';
-import { RestaurantStatsController } from './controllers/restaurant-stats.controller';
+// import { RestaurantStatsController } from './controllers/restaurant-stats.controller';
 import { BuildingModule } from 'src/building/building.module';
 
 @Module({
@@ -15,7 +15,7 @@ import { BuildingModule } from 'src/building/building.module';
     AuthModule,
     BuildingModule,
   ],
-  controllers: [RestaurantStatsController],
+  // controllers: [RestaurantStatsController],
   providers: [RestaurantStatsService],
   exports: [TypeOrmModule, RestaurantStatsService],
 })
